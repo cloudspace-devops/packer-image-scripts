4bd82648933b930495c3b91a88215be9b8874766
Matthew Cupples <mcupples@cloudspace.com>
2014-05-21 17:22:36 -0400

    forgot sudo

--- scripts/install-virtualbox-guest-additions.sh
+++ scripts/install-virtualbox-guest-additions.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Mount the disk image
-cd /tmp
-mkdir /tmp/isomount
-mount -t iso9660 -o loop /home/cloudspace/VBoxGuestAdditions.iso /tmp/isomount
+sudo cd /tmp
+sudo mkdir /tmp/isomount
+sudo mount -t iso9660 -o loop /home/cloudspace/VBoxGuestAdditions.iso /tmp/isomount
 
 # Install the drivers
-/tmp/isomount/VBoxLinuxAdditions.run
+sudo /tmp/isomount/VBoxLinuxAdditions.run
 
 # Cleanup
-umount isomount
-rm -rf isomount /home/cloudspace/VBoxGuestAdditions.iso
+sudo umount isomount
+sudo rm -rf isomount /home/cloudspace/VBoxGuestAdditions.iso
 
 # from: https://github.com/gwagner/packer-centos/blob/master/provisioners/install-virtualbox-guest-additions.sh
 
