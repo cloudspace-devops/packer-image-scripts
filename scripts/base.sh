51c7df27a0fbc995c2b825f892dd899a1c2128a0
Matthew Cupples <mcupples@cloudspace.com>
2014-05-21 10:25:17 -0400

    added generic installs

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,6 +4,6 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Install necessary libraries for guest additions and Vagrant NFS Share
-sudo apt-get -y install linux-headers-$(uname -r) build-essential dkms nfs-common
+sudo apt-get -y install linux-headers-$(uname -r) build-essential dkms nfs-common git-core whois traceroute htop unzip sysstat mailutils linux-headers-generic vim
 
 sudo apt-get clean
