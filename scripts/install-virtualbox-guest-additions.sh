e8a62176baf0670a01e7879f82b21cd01d7517aa
Matthew Cupples <mcupples@cloudspace.com>
2014-05-22 13:08:27 -0400

    change cloudspace to vagrant

--- scripts/install-virtualbox-guest-additions.sh
+++ scripts/install-virtualbox-guest-additions.sh
@@ -3,14 +3,14 @@
 # Mount the disk image
 sudo cd /tmp
 sudo mkdir /tmp/isomount
-sudo mount -t iso9660 -o loop /home/cloudspace/VBoxGuestAdditions_{{.Version}}.iso /tmp/isomount
+sudo mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions_{{.Version}}.iso /tmp/isomount
 
 # Install the drivers
 sudo /tmp/isomount/VBoxLinuxAdditions.run
 
 # Cleanup
 sudo umount isomount
-sudo rm -rf isomount /home/cloudspace/VBoxGuestAdditions.iso
+sudo rm -rf isomount /home/vagrant/VBoxGuestAdditions.iso
 
 # from: https://github.com/gwagner/packer-centos/blob/master/provisioners/install-virtualbox-guest-additions.sh
 
